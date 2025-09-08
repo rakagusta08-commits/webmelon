67d66b45ab2887c8cb0d56c5b56730990b5af67e
rakagusta08-commits <rakagusta08@gmail.com>
2025-09-08 07:40:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
